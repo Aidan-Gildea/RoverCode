b7c9e176bea04eb889eb91f0adfc3fc99e5ffda7
Linc <lincsung906988@gmail.com>
2025-07-18 15:01:52 -0700

    I've made the changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <Arduino.h>
-
+#include <secondheader.h>
 
 // testing testing : )
+const int frontLeftTrig = 2, frontLeftEcho = 3;
+const int frontRightTrig = 4, frontRightEcho = 5;
+const int leftTrig = 6, leftEcho = 7;
+const int rightTrig = 8, rightEcho = 9;
+
+// Constants
+const int closeDistance = 10; // cm
+const int mazeWidth = 30;
+const int robotWidth = 10;
+
 
 void setup() {
+  Serial.begin(9600);
 
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
 
 }
-
