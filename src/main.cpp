9fe14b5e32625c5e8e223b498265a9ae128ca196
Adiad <apgildea@gmail.com>
2025-07-21 13:05:23 -0700

    1:05pm

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,55 @@
 #include <Arduino.h>
-#include <secondheader.h>
+#include <test.h>
 
-// testing testing : )
-const int frontLeftTrig = 2, frontLeftEcho = 3;
-const int frontRightTrig = 4, frontRightEcho = 5;
-const int leftTrig = 6, leftEcho = 7;
-const int rightTrig = 8, rightEcho = 9;
+#define sensorPin 12
 
-// Constants
-const int closeDistance = 10; // cm
-const int mazeWidth = 30;
-const int robotWidth = 10;
+#define enA_1 7
+#define in1_1 6
+#define in2_1 5
+
+HBridge hBridge(enA_1, in1_1, in2_1);
+
+
+int currentState;
+int previousState;
+volatile int tickCount;
+
+unsigned long lastTickTime;
+unsigned long currentTickTime;
+unsigned long deltaTime;
+float rpm;
 
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(sensorPin, INPUT);
+
+  hBridge.setMotorSpeed(255);
+  hBridge.driveForward();
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  currentState = digitalRead(sensorPin);
+
+  if (currentState != previousState) {
+    if (currentState == HIGH) {
+      tickCount++;
+      currentTickTime = millis();
+      deltaTime = currentTickTime - lastTickTime;
+      lastTickTime = currentTickTime;
+
+      rpm = (tickCount / (deltaTime / 60000.0)); // Convert to RPM
+      Serial.print("RPM: ");
+      Serial.println(rpm);
+    }
+  }
+
+  previousState = currentState;
+
+  delay(100); // Adjust delay as needed
+  
 
 }
